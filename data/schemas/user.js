261ffff9570b590631d0a59d3311b8341934b626
lucasprus <lucas.prus@gmail.com>
2013-06-17 22:49:32 +0100

    Commit 4

--- data/schemas/user.js
+++ data/schemas/user.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
+var request = require('request');
 
 var emailRegexp = /.+\@.+\..+/;
 
 var UserSchema = new mongoose.Schema({
   username: {type: String, unique: true},
-  name: String,
+name: mongoose.Schema.Types.Mixed,
   password: String,
   email: {
     type: String,
@@ -16,7 +17,75 @@ var UserSchema = new mongoose.Schema({
     required: true,
     uppercase: true,
     'enum': ['M', 'F']
-  }
+	},
+	birthday: {
+	type: Date,
+	validate: [
+	validate_18_years_old_or_more,
+	'You must be 18 years old or more']
+},
+twitter: {
+type: String,
+validate: [twitterHandleExists, 'Please provide a valid twitter handle'],
+set: filterTwitterHandle,
+get: filterTwitterHandle
+}
+});
+
+UserSchema
+.virtual('twitter_url')
+.get(function() {
+if (this.twitter) {
+return 'http://twitter.com/' + encodeURIComponent(this.twitter);
+}
+});
+
+UserSchema
+.virtual('full_name')
+.get(function() {
+if (typeof this.name === 'string') {
+return this.name;
+}
+return [this.name.first, this.name.last].join(' ');
+})
+.set(function(fullName) {
+var nameComponents = fullName.split(' ');
+this.name = {
+last: nameComponents.pop(),
+first: nameComponents.join(' ')
+};
 });
 
 module.exports = UserSchema;
+
+var TIMESPAN_YEAR = 31536000000;
+var TIMESPAN_18_YEARS = 18 * TIMESPAN_YEAR;
+
+function validate_18_years_old_or_more(date) {
+return (Date.now() - date.getTime()) > TIMESPAN_18_YEARS;
+}
+
+function twitterHandleExists(handle, done) {
+	request('http://twitter.com/' + encodeURIComponent(handle), function(err, res) {
+		if (err) {
+			console.error(err);
+			return done(false);
+		}
+		if (res.statusCode > 299) {
+			done(false);
+		} else {
+			done(true);
+		}
+	});
+}
+
+function filterTwitterHandle(handle) {
+	if (! handle) {
+	return;
+	}
+	handle = handle.trim();
+	if (handle.indexOf('@') === 0) {
+	handle = handle.substring(1);
+	}
+	return handle;
+}
